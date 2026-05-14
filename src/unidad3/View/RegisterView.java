cdd059ce0e74bc0defd991bb25da61cb09031eaf
darnellchingon <darnella_23@alu.uabcs.mx>
2026-05-14 14:51:40 -0700

    nasty

--- src/unidad3/View/RegisterView.java
+++ src/unidad3/View/RegisterView.java
@@ -1,21 +1,81 @@
 package unidad3.View;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class RegisterView extends JFrame {
+import unidad3.Model.AuthModel;
 
-    public RegisterView() {
-        setTitle("Registro");
-        setSize(300, 200);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+public class RegisterView {
 
-        JLabel label = new JLabel("Vista de Registro funcionando");
-        add(label);
+    JFrame ventana;
+    JTextField txtUser;
+    JPasswordField txtPass;
+    JButton btnGuardar;
+    JButton btnCancelar;
 
-        setVisible(true);
+    UserView userView; 
 
-        System.out.println("RegisterView abierta correctamente");
+    public RegisterView(UserView userView) {
+
+        this.userView = userView;
+
+        ventana = new JFrame("Registro");
+        ventana.setSize(400,300);
+        ventana.setLayout(null);
+        ventana.setLocationRelativeTo(null);
+
+        JLabel l1 = new JLabel("Usuario");
+        l1.setBounds(50,50,100,20);
+        ventana.add(l1);
+
+        txtUser = new JTextField();
+        txtUser.setBounds(50,70,300,30);
+        ventana.add(txtUser);
+
+        JLabel l2 = new JLabel("Password");
+        l2.setBounds(50,110,100,20);
+        ventana.add(l2);
+
+        txtPass = new JPasswordField();
+        txtPass.setBounds(50,130,300,30);
+        ventana.add(txtPass);
+
+        btnGuardar = new JButton("Guardar");
+        btnGuardar.setBounds(50,180,120,30);
+        ventana.add(btnGuardar);
+
+        btnCancelar = new JButton("Cancelar");
+        btnCancelar.setBounds(200,180,120,30);
+        ventana.add(btnCancelar);
+
+        // BOTON GUARDAR
+        btnGuardar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                String user = txtUser.getText();
+                String pass = new String(txtPass.getPassword());
+
+                AuthModel model = new AuthModel();
+
+                boolean ok = model.register(user, pass, user);
+
+                if(ok){
+                    JOptionPane.showMessageDialog(ventana, "Registrado");
+
+                    ventana.dispose();
+
+                    userView.cargarTabla();
+
+                } else {
+                    JOptionPane.showMessageDialog(ventana, "Error");
+                }
+            }
+        });
+
+        // BOTON CANCELAR
+        btnCancelar.addActionListener(e -> ventana.dispose());
+
+        ventana.setVisible(true);
     }
 }
